63abc4e8a4d4d837a91e4e03426533049398418b
hermes7144 <kichancho@gmail.com>
2023-02-09 08:57:25 +0900

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
+
+export default function Navbar() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
+  return (
+    <header className='flex justify-between border-b border-gray-300 p-2'>
+      <Link to='/' className='flex items-center text-4xl text-brand'>
+        <h1>DH Calc.</h1>
+      </Link>
+      <nav className='flex items-center gap-4 font-semibold'>
+        <Link to='/invest'>Invest</Link>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>LogOut</button>}
+      </nav>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Invest from './pages/Invest';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: <NotFound />,
+  children: [
+    { index: true, path: '/', element: <Home /> },
+    {
+      path: '/invest',
+      element: <Invest />
+    }
+  ]
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
