f7e267fd871bdbb54c9bf68dff96a34a6b037825
hermes7144 <kichancho@gmail.com>
2023-02-12 14:20:47 +0900

    feat:리액트 단위 테스트 추가

--- src/pages/Invest.jsx
+++ src/pages/Invest.jsx
@@ -13,8 +13,6 @@ const numList = {
   9: 450000,
 };
 
-const errorText = '올바른 값을 입력해주세요.';
-
 const Rapael = 1.25;
 const nonMonopoly = 2;
 
@@ -23,52 +21,49 @@ export default function Invest() {
   const [price, setPrice] = useState();
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const name = e.target.name;
+    const value = e.target.value.replace(/\D/g, '');
+
     setInvest((invest) => ({ ...invest, [name]: value }));
   };
 
   const getMoney = (input) => {
     const firstNumber = Math.floor(input / 1000);
-    const remainder = input - firstNumber * 1000;
-    return numList[firstNumber] + (firstNumber || 1) * 10 * remainder;
+    const leftNumber = input - firstNumber * 1000;
+    return numList[firstNumber] + (firstNumber || 1) * 10 * leftNumber;
   };
 
   const getRequestMoney = useCallback(() => {
-    return getMoney(invest.target) - getMoney(invest.current);
+    invest.target && invest.current ? setPrice(getMoney(invest.target) - getMoney(invest.current)) : setPrice(null);
   }, [invest]);
 
   const getCount = (money) => getCountText(money / (invest.current * 10));
   const getCountText = (money) => `${parseInt(money)}번 ${Math.ceil((money % 1) * 20)}칸`;
 
   useEffect(() => {
-    if (invest.target && invest.current) {
-      setPrice(getRequestMoney());
-    } else {
-      setPrice(null);
-    }
-  }, [invest, setPrice, getRequestMoney]);
-
+    getRequestMoney();
+  });
   return (
     <div className='flex'>
       <article className='w-full basis-1/2'>
         <h2 className='text-2xl font-bold my-4 text-center'>Calculator</h2>
         <form className='flex flex-col px-12'>
           <label htmlFor='target'>목표값</label>
-          <input id='target' name='target' type='text' pattern='\d*' maxLength='4' value={invest.target ?? ''} onChange={handleChange} />
+          <input id='target' name='target' type='text' maxLength='4' value={invest.target} aria-label='target' onChange={handleChange} />
           <label htmlFor='current'>현재값</label>
-          <input id='current' name='current' type='text' pattern='\d*' maxLength='4' value={invest.current ?? ''} onChange={handleChange} />
+          <input id='current' name='current' type='text' maxLength='4' value={invest.current} aria-label='current' onChange={handleChange} />
           <label>필요금액(독점)</label>
-          <input value={price || errorText} disabled />
+          <input value={price || ''} aria-label='moneyMono' disabled />
           <label>필요금액(비독점)</label>
-          <input value={price * nonMonopoly || errorText} disabled />
+          <input value={price * nonMonopoly || ''} aria-label='moneyNonMono' disabled />
           <label>투자횟수(독점)</label>
-          <input value={(price && getCount(price)) || errorText} disabled />
+          <input value={(price && getCount(price)) || ''} aria-label='countMono' disabled />
           <label>투자횟수(비독점)</label>
-          <input value={(price && getCount(price * nonMonopoly)) || errorText} disabled />
+          <input value={(price && getCount(price * nonMonopoly)) || ''} aria-label='countNonMono' disabled />
           <label>라파엘 군사투자(독점)</label>
-          <input value={(price && getCount(price * Rapael)) || errorText} disabled />
+          <input value={(price && getCount(price * Rapael)) || ''} aria-label='rapaelMono' disabled />
           <label>라파엘 군사투자(비독점)</label>
-          <input value={(price && getCount(price * Rapael * nonMonopoly)) || errorText} disabled />
+          <input value={(price && getCount(price * Rapael * nonMonopoly)) || ''} aria-label='rapaelNonMono' disabled />
         </form>
       </article>
     </div>
