552f4a5d8fcbd20cbc68613c1d2562494eb6d7e6
hermes7144 <kichancho@gmail.com>
2023-02-17 19:53:27 +0900

    feat:CI warning 제거

--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -5,7 +5,6 @@ import Button from '../components/ui/Button';
 export default function NewItem() {
   const [item, setItem] = useState({});
   const [isUploading, setIsUploading] = useState(false);
-  const [success, setSuccess] = useState();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,7 +20,6 @@ export default function NewItem() {
     <>
       <article className='w-full basis-1/2'>
         <h2 className='text-2xl font-bold my-4 text-center'>새로운 아이템 등록</h2>
-        {success && <p className='my-2'>✔️{success}</p>}
 
         <form className='flex flex-col px-12' onSubmit={handleSubmit}>
           <label htmlFor='category'>카테고리</label>
